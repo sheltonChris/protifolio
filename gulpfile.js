607113380088c3298319f8cbdc7c246ea3c95fd9
Shelton Cristian da Costa Werly <sheltonwarrior@outlook.com>
2025-07-07 22:41:48 -0300

    primeiro upload

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate')
+
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
+}
+
+function comprimeImagens() {
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'))
+}
+
+function comprimeJS() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
+
+exports.default = gulp.parallel(compilaSass, comprimeImagens, comprimeJS);
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', gulp.parallel(compilaSass))
+    gulp.watch('./source/scripts/*js', gulp.parallel(comprimeJS))
+}
